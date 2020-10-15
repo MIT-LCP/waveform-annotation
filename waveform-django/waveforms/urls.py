2e16defec4e366c7c4b38d8711917e5533902972
Lucas McCullum <lumccul3@gmail.com>
2020-10-15 11:49:30 -0400

    Adds annotation display table

--- waveform-django/waveforms/urls.py
+++ waveform-django/waveforms/urls.py
@@ -6,4 +6,5 @@ from waveforms import views
 urlpatterns = [
     path('django_plotly_dash/', include('django_plotly_dash.urls')),
     path('', views.waveform_published_home, name='waveform_published_home'),
+    path('annotations', views.render_annotations, name='render_annotations'),
 ]
--- waveform-django/waveforms/views.py
+++ waveform-django/waveforms/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from waveforms.models import Annotation
 
 
 def waveform_published_home(request):
@@ -17,3 +18,42 @@ def waveform_published_home(request):
     """
 
     return render(request, 'waveforms/home.html')
+
+
+def render_annotations(request):
+    """
+    Render all saved annotations to allow edits.
+
+    Parameters
+    ----------
+    N/A : N/A
+
+    Returns
+    -------
+    N/A : HTML page / template variable
+        HTML webpage responsible for displaying the annotations.
+
+    """
+    all_annotations = Annotation.objects.all()
+
+    categories = [
+        'project',
+        'record',
+        'decision',
+        'comments',
+        'decision_date'
+    ]
+    values = []
+
+    for ann in all_annotations:
+        temp_values = []
+        temp_values.append(ann.project)
+        temp_values.append(ann.record)
+        temp_values.append(ann.decision)
+        temp_values.append(ann.comments)
+        temp_values.append(ann.decision_date)
+        values.append(temp_values)
+
+    return render(request, 'waveforms/annotations.html',
+        {'categories': categories,
+         'values': values})
