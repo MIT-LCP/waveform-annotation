e091b26fa75850a1ac3371b669db1ad26022b688
Lucas McCullum <lumccul3@gmail.com>
2020-10-19 15:54:57 -0400

    Conserves previous annotation results

--- waveform-django/export/schema.py
+++ waveform-django/export/schema.py
@@ -1,18 +1,19 @@
 import graphene
 from graphene_django.types import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 
 from waveforms.models import Annotation
+
 
 # View results at:
 # http://localhost:8000/graphql?query={all_annotations{project,record,decision,comments,decision_date}}
 class AnnotationType(DjangoObjectType):
     class Meta:
         model = Annotation
-        fields = ('project', 'record', 'decision', 'comments',
-                  'decision_date')
+        filter_fields = ['project', 'record', 'decision', 'comments',
+                         'decision_date']
+        interfaces = (graphene.relay.Node, )
 
-class Query(object):
-    all_annotations = graphene.List(AnnotationType)
 
-    def resolve_all_annotations(self, info, **kwargs):
-        return Annotation.objects.all()
+class Query(graphene.ObjectType):
+    all_annotations = DjangoFilterConnectionField(AnnotationType)
