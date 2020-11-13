08d86ae3a0db923f6acdba70f8790cf1f92db4f5
Lucas McCullum <lumccul3@gmail.com>
2020-11-13 14:12:05 -0500

    Reconfigures settings; adds optional debug toolbar

--- waveform-django/website/urls.py
+++ waveform-django/website/urls.py
@@ -1,20 +1,23 @@
 from django.conf.urls import include
 from django.urls import path
 from django.http import HttpResponse
+import debug_toolbar
 
 from website import views
 
 
 urlpatterns = [
+    # Debug toolbar
+    path('__debug__/', include(debug_toolbar.urls)),
     # Account creation and handling pages
-	path('register/', views.register_page, name='register'),
-	path('login/', views.login_page, name='login'),
-	path('logout/', views.logout_user, name='logout'),
+	path('waveform-annotation/register/', views.register_page, name='register'),
+	path('waveform-annotation/login/', views.login_page, name='login'),
+	path('waveform-annotation/logout/', views.logout_user, name='logout'),
     # Waveform annotator dashboard
-    path('waveforms/', include('waveforms.urls')),
+    path('waveform-annotation/waveforms/', include('waveforms.urls')),
     # GraphQL API interface
-    path('', include('export.urls')),
+    path('waveform-annotation/', include('export.urls')),
     # Robots.txt for crawlers
-    path('robots.txt', lambda x: HttpResponse('User-Agent: *\Allow: /',
+    path('waveform-annotation/robots.txt', lambda x: HttpResponse('User-Agent: *\Allow: /',
         content_type='text/plain'), name='robots_file'),
 ]
