d5cf894de3bf5150cee2cbcbb054c19e469b74f5
Lucas McCullum <lumccul3@gmail.com>
2021-03-04 11:37:09 -0500

    Adds customizable graph settings

--- waveform-django/waveforms/models.py
+++ waveform-django/waveforms/models.py
@@ -21,3 +21,25 @@ class Annotation(models.Model):
                 a.save(update_fields=['decision', 'comments', 'decision_date'])
         if not exists_already:
             self.save()
+
+
+class UserSettings(models.Model):
+    user = models.CharField(max_length=150, unique=True, blank=False, default='')
+    fig_height = models.FloatField(blank=False, default=725.0)
+    fig_width = models.FloatField(blank=False, default=875.0)
+    margin_left = models.FloatField(blank=False, default=0.0)
+    margin_top = models.FloatField(blank=False, default=25.0)
+    margin_right = models.FloatField(blank=False, default=0.0)
+    margin_bottom = models.FloatField(blank=False, default=0.0)
+    grid_color = models.TextField(blank=False, default='#ff3c3c')
+    sig_color = models.TextField(blank=False, default='#000000')
+    sig_thickness = models.FloatField(blank=False, default=1.5)
+    ann_color = models.TextField(blank=False, default='#3c3cc8')
+    grid_delta_major = models.FloatField(blank=False, default=0.2)
+    max_y_labels = models.IntegerField(blank=False, default=8)
+    down_sample_ekg = models.IntegerField(blank=False, default=8)
+    down_sample = models.IntegerField(blank=False, default=16)
+    time_range_min = models.FloatField(blank=False, default=40.0)
+    time_range_max = models.FloatField(blank=False, default=10.0)
+    window_size_min = models.FloatField(blank=False, default=10.0)
+    window_size_max = models.FloatField(blank=False, default=1.0)
