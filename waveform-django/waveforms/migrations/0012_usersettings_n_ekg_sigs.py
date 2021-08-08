f0ef4d5c3e47a5fbd2f7d7071aa83fb8de5c8c3f
Lucas McCullum <lumccul3@gmail.com>
2021-08-08 17:39:52 -0400

    Fixes migration merge conflict

--- waveform-django/waveforms/migrations/0012_usersettings_n_ekg_sigs.py
+++ waveform-django/waveforms/migrations/0012_usersettings_n_ekg_sigs.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('waveforms', '0009_usersettings_signal_std'),
+        ('waveforms', '0011_invitedemails'),
     ]
 
     operations = [
