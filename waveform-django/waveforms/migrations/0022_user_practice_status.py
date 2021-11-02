8473e48af62528b7076d56842d669de8f550946f
root <root@webserver.ecg.mit.edu>
2021-11-02 11:45:02 -0400

    Fixes migrations

--- waveform-django/waveforms/migrations/0022_user_practice_status.py
+++ waveform-django/waveforms/migrations/0022_user_practice_status.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('waveforms', '0020_auto_20211025_1539'),
+        ('waveforms', '0021_auto_20211030_1812'),
     ]
 
     operations = [
