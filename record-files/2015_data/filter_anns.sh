2986b13ac68dacdcaed802ad23aaef9dd2239757
root <root@webserver.ecg.mit.edu>
2021-12-08 02:40:30 -0500

    Fixes bugs in annotation filter

--- record-files/2015_data/filter_anns.sh
+++ record-files/2015_data/filter_anns.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # All the desired types of annotations
-ann_types="VTach\|VFib"
-out_record_name="RECORDS_VTVF"
-saved_recs=$(tail -n +2 all_anns.csv | cut -d , -f3-4)
+out_record_name="RECORDS_VTVF_LIMIT-5"
+limit=5
+saved_events=$(tail -n +2 all_anns.csv | cut -d , -f4 | sort | uniq)
 
 # Get all of the folders with the desired annotation type
-folders=$(grep -r --include="*.hea" "$ann_types" . | cut -d : -f1 | cut -d / -f2 | uniq | sort -n -t e -k2)
+folders=$(grep -r --include="*.hea" . | cut -d : -f1 | cut -d / -f1 | uniq | sort -n -t e -k2)
 echo $folders | tr " " "\n" > $out_record_name
 
 # Clear list of events that can be assigned
@@ -14,35 +14,43 @@ for folder in $folders; do
 done
 
 # Give priority to events that already have annotations
-for info in $saved_recs; do
-    rec=$(echo $info | cut -d , -f1)
-    event=$(echo $info | cut -d , -f2)
-    echo $event >> $rec/$out_record_name
+saved_events=($saved_events)
+for folder in $folders; do
+    rec_events=()
+    for e in "${saved_events[@]}"; do [[ $e == "$folder"* ]] && rec_events+=("$e"); done
+    shuffled_events=$(shuf -n $limit -e ${rec_events[@]} | sort)
+    for event in $shuffled_events; do
+        echo $event >> $folder/$out_record_name
+    done
 done
 
 # Randomly select events such that only
 for folder in $folders; do
     # Create a new RECORDS file for each subfolder
     lines=$(wc -l $folder/$out_record_name | cut -d ' ' -f1)
-    limit=6
-    if [ $lines -lt $limit ]; then
-        add=$(expr 6 - $lines)
+    if [ $lines -le $limit ]; then
+        add=$(expr $limit - $lines + 1)
         events=$(grep -ir --include="*.hea" "$ann_types" $folder | cut -d : -f1 | cut -d / -f2 | cut -d . -f1 | sort -n -t _ -k2 | uniq)
-        if [ $lines -eq 1 ]; then
-            shuf -n $add -e $events >> $folder/$out_record_name
-        else
+	n_events=$(echo $events | tr " " "\n" | wc -l)
+	if [[ $lines -eq 1 || $n_events -le $limit ]]; then
+            echo $folder > $folder/$out_record_name
+            shuf -n $add -e $events | sort >> $folder/$out_record_name
+        else    
             present=$(tail -n +2 $folder/$out_record_name)
-            rand=$(shuf -n $add -e $events)
+	    rand=$(shuf -n $add -e $events)
             assign_set=$(echo $present $rand)
             repeats=$(echo $assign_set | tr ' ' '\n' | sort -rn | uniq -c | sort -rn | head -n 1 | cut -d' ' -f7)
-            
             while [ $repeats -gt 1 ]; do
-                rand=$(shuf -n $add -e $events)
+		rand=$(shuf -n $add -e $events)
                 assign_set=$(echo $present $rand)
-                repeats=$(echo $assign_set | tr ' ' '\n' | sort -rn | uniq -c | sort -rn | head -n 1 | cut -d' ' -f7)
-            done
+		assign_len=$(echo $assign_set | wc -w)
+                if [ $assign_len -le $limit ]; then
+		    break
+		fi
+		repeats=$(echo $assign_set | tr ' ' '\n' | sort -rn | uniq -c | sort -rn | head -n 1 | cut -d' ' -f7)
+	    done
 
-            echo $rand | tr ' ' '\n' | sort >> $folder/$out_record_name
+	    echo $rand | tr ' ' '\n' | sort >> $folder/$out_record_name
 
         fi
     fi
--- record-files/2021_data/filter_anns.sh
+++ record-files/2021_data/filter_anns.sh
@@ -1,14 +1,58 @@
 #!/bin/sh
 # All the desired types of annotations
-ann_types="VTach\|VFib"
-out_record_name="RECORDS_VTVF"
+out_record_name="RECORDS_VTVF_LIMIT-5"
+limit=5
+saved_events=$(tail -n +2 all_anns.csv | cut -d , -f4 | sort | uniq)
 
 # Get all of the folders with the desired annotation type
-folders=$(grep -r --include="*.hea" "$ann_types" . | cut -d : -f1 | cut -d / -f2 | uniq | sort -n -t e -k2)
-echo $folders | tr " " "\n" >> $out_record_name
+folders=$(grep -r --include="*.hea" . | cut -d : -f1 | cut -d / -f1 | uniq | sort -n -t e -k2)
+echo $folders | tr " " "\n" > $out_record_name
+
+# Clear list of events that can be assigned
+for folder in $folders; do
+    echo $folder > $folder/$out_record_name
+done
+
+# Give priority to events that already have annotations
+saved_events=($saved_events)
+for folder in $folders; do
+    rec_events=()
+    for e in "${saved_events[@]}"; do [[ $e == "$folder"* ]] && rec_events+=("$e"); done
+    shuffled_events=$(shuf -n $limit -e ${rec_events[@]} | sort)
+    for event in $shuffled_events; do
+        echo $event >> $folder/$out_record_name
+    done
+done
+
+# Randomly select events such that only
 for folder in $folders; do
     # Create a new RECORDS file for each subfolder
-    grep -ir --include="*.hea" "$ann_types" $folder | cut -d : -f1 | cut -d / -f2 | cut -d . -f1 | uniq | sort -n -t _ -k2 | shuf -n 5 > $folder/$out_record_name
+    lines=$(wc -l $folder/$out_record_name | cut -d ' ' -f1)
+    if [ $lines -le $limit ]; then
+        add=$(expr $limit - $lines + 1)
+        events=$(grep -ir --include="*.hea" "$ann_types" $folder | cut -d : -f1 | cut -d / -f2 | cut -d . -f1 | sort -n -t _ -k2 | uniq)
+	n_events=$(echo $events | tr " " "\n" | wc -l)
+	if [[ $lines -eq 1 || $n_events -le $limit ]]; then
+            echo $folder > $folder/$out_record_name
+            shuf -n $add -e $events | sort >> $folder/$out_record_name
+        else    
+            present=$(tail -n +2 $folder/$out_record_name)
+	    rand=$(shuf -n $add -e $events)
+            assign_set=$(echo $present $rand)
+            repeats=$(echo $assign_set | tr ' ' '\n' | sort -rn | uniq -c | sort -rn | head -n 1 | cut -d' ' -f7)
+            while [ $repeats -gt 1 ]; do
+		rand=$(shuf -n $add -e $events)
+                assign_set=$(echo $present $rand)
+		assign_len=$(echo $assign_set | wc -w)
+                if [ $assign_len -le $limit ]; then
+		    break
+		fi
+		repeats=$(echo $assign_set | tr ' ' '\n' | sort -rn | uniq -c | sort -rn | head -n 1 | cut -d' ' -f7)
+	    done
+
+	    echo $rand | tr ' ' '\n' | sort >> $folder/$out_record_name
+
+        fi
+    fi
     # Add the folder name to the top of the RECORDS file
-    echo $folder | cat - $folder/$out_record_name > temp && mv temp $folder/$out_record_name
 done
