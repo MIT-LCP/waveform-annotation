85f199f421ea0475eddc73bdd46fabeb494afd50
Lucas McCullum <lumccul3@gmail.com>
2023-01-22 00:55:06 -0600

    Adds new sample data / cleans up

--- record-files/2021_data/subfolder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Create subfolders based on record name
-for file in *;
-do
-    dir = $(echo $file | cut -d"_" -f1);
-    echo $dir;
-    mkdir -p $dir;
-    mv $file $dir;
-done
--- record-files/2021_data/top_records.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Create top-level RECORDS file based on folders
-records_file="RECORDS_VTVF_LIMIT-5"
-rm -f $records_file
-for folder in `ls -d */ | sort -n -k1.3`; do
-    echo ${folder%%/}
-done > $records_file
